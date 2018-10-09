4de85ce5a94d2cbbbede721163eeb91423880fb0
poikelp <takukelp@gmail.com>
2018-10-10 00:43:32 +0900

    firebase導入

--- docs/JS/output.js
+++ docs/JS/output.js
@@ -1,3 +1,17 @@
 $("#output").on("click", function(){
-    alert("hoge");
+    Upload();
 })
+
+function getCanvasPng(){
+    return $("#canvas").get(0).toDataURL();
+}
+
+function Upload(){
+    var date = new Date();
+    var filename = "images/" + date.toString() + ".png";
+
+    var storageRef = firebase.storage().ref(filename);
+    storageRef.put(getCanvasPng()).then(function(){
+        console.log("hoge");
+    });
+}
