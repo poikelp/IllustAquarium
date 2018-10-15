247238e5342165033b25c0a5882ab4f53c078a8f
poikelp <takukelp@gmail.com>
2018-10-15 20:18:10 +0900

    画像アップロードできた！天才！

--- docs/JS/output.js
+++ docs/JS/output.js
@@ -12,16 +12,31 @@ function Upload(){
     // console.log(file);
 
     var storageRef = firebase.storage().ref();
-    var mountainsRef = storageRef.child("images");
+    // var mountainsRef = storageRef.child("images/");
     var mountainImagesRef = storageRef.child(fullpath);
 
     var canvas = $("#canvas").get(0);
 
-    var file = new Blob;
-    canvas.toBlob(function(blob){
-        // file.src = URL.createObjectURL(blob);
-        file = blob;
-    });
+   
+    // // var file = new Blob();
+    // canvas.toBlob(function(blob){
+    //     var file = new Image();
+    //     file.src = URL.createObjectURL(blob);
+    //     // file = blob;
+    //     // file.src = blob;
+    //     mountainImagesRef.put(file);
+
+    // },"image/png");
+
+    var type = "image/png";
+    var dataurl = canvas.toDataURL(type);
+    // var bin = btoa(dataurl.split(',')[1]);
+    var bin = atob(dataurl.replace(/^.*,/, ''));
+    var buffer = new Uint8Array(bin.length);
+    for (i=0;i<bin.length;i++){
+        buffer[i] = bin.charCodeAt(i);
+    }
+    var file = new Blob([buffer.buffer], {type: type});
 
     mountainImagesRef.put(file);
 
